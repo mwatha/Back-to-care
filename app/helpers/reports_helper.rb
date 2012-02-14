0cf0604c3a9892d13c767365e6ef5e234183b0b3
mwatha <mwatha.bwanalie@baobabhealth.org>
2012-02-14 16:08:47 +0200

    adding a new report - current sms sent status

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -36,4 +36,48 @@ EOF
     @html+='</tbody></table>'   
   end
 
+  def sms_status_table(patients , title)
+    @html =<<EOF
+<table id="patient_data">
+<thead>                                                                         
+<tr id = 'table_head_tr'>                                                          
+<th id="th1">#{"&nbsp;"*2}National ID</th>                                  
+<th id="th2">First name</th>                             
+<th id="th3">Last name</th>                              
+<th id="th4">Sex</th>                                     
+<th id="th5">DOB</th>                                    
+<th id="th6">Message</th>                        
+<th id="th7">Sent to</th>                        
+<th id="th7">Sender</th>                        
+<th id="th9">Sent date</th>                        
+<th id="th10">Delivered</th>                        
+<th id="th11">&nbsp;</th>                                 
+</tr>                                                                           
+</thead>
+<tbody>
+EOF
+
+    patients.each do | demographics |
+      @html+=<<EOF
+<tr class="patient_info" style="vertical-align: top;">                                                       
+  <td>#{"&nbsp;"*2}#{demographics[:national_id]}</td>                                                      
+  <td>#{demographics[:first_name]}</td>                                                                     
+  <td>#{demographics[:last_name]}</td>                                                                     
+  <td>#{demographics[:gender]}</td>                                                                     
+  <td>#{demographics[:dob]}</td>                                                                     
+  <td>#{demographics[:message]}</td>                                                                     
+  <td>#{demographics[:number]}</td>                                                                     
+  <td>#{demographics[:sender]}</td>                                                                     
+  <td>#{demographics[:send_date].to_date rescue nil}</td>                                                                     
+  <td>#{demographics[:status]}</td>                                                                     
+  <td><a href="/issues/show?id=#{demographics[:person_id]}&title=#{title}" class="show_details">Show</a>|
+  <a href="/issues/sms/#{demographics[:person_id]}" class="show_details">Send sms</a>
+  </td>                                                                     
+</tr>                                                                           
+EOF
+    end
+  
+    @html+='</tbody></table>'   
+  end
+
 end
--- lib/patient_service.rb
+++ lib/patient_service.rb
@@ -199,8 +199,13 @@ module PatientService
           patients << self.demographics(Person.find(patient_id))
         end
       when 'SMS status'
-        self.sms_status(start_date,end_date).map do |patient_id|
-          patients << self.demographics(Person.find(patient_id))
+        self.sms_status(start_date,end_date).map do |record|
+          patients << {:person_id => record[0] ,:gender => record[3] ,                                            
+                       :dob => record[4] , :first_name => record[1] ,                                
+                       :last_name => record[2],:national_id => record[7],
+                       :send_date => record[5],
+                       :message => record[9],:sender => record[8],
+                       :number => record[10],:status => record[11] }
         end
       when 'Missed appointments'
         self.missed_appointment(start_date,end_date).map do |record|
@@ -229,8 +234,41 @@ module PatientService
   protected
 
   def self.sms_status(start_date,end_date)                                 
-    #Sms.find(:all,:joins =>"INNER JOIN person_names n ON sms.person_id = n.person_id
-    #INNER JOIN "                        
+    db_name = YAML.load(File.open(File.join(RAILS_ROOT, "config/database.yml"), "r"))["bart"]["database"]
+    sms_type_id = SmsType.find_by_name("Outbox").id
+    records = Sms.find(:all,:joins =>"INNER JOIN #{db_name}.person p 
+      ON p.person_id = sms.person_id
+      INNER JOIN #{db_name}.person_name n ON p.person_id=n.person_id                                 
+      INNER JOIN #{db_name}.patient_identifier i ON i.patient_id = sms.person_id 
+      AND i.identifier_type = (SELECT patient_identifier_type_id 
+      FROM #{db_name}.patient_identifier_type pi WHERE pi.name = 'National id')
+      INNER JOIN #{db_name}.users u ON u.user_id=sms.provider_id",
+      :conditions =>["sms_type_id=?",sms_type_id],
+      :select =>"sms.person_id,n.given_name,n.family_name, p.gender, 
+      p.birthdate , sms.message , sms.delivered , sms.number, sms.date_created sent_date ,
+      u.username sender,i.identifier national_id",
+      :order => "sms.date_created DESC")
+
+    (records || []).collect do |r|
+      [r.person_id,r.given_name , r.family_name , r.gender,
+       r.birthdate , r.sent_date , r.delivered ,r.national_id,
+       r.sender , r.message , r.number,r.delivered]
+    end
+=begin
+FROM obs 
+INNER JOIN encounter e ON e.encounter_id = obs.encounter_id 
+AND e.encounter_datetime=(SELECT MAX(x.encounter_datetime) FROM encounter x WHERE x.patient_id=e.patient_id)
+INNER JOIN drug d ON d.drug_id = obs.value_drug and d.concept_id IN(SELECT s.concept_id FROM concept_set s WHERE (concept_set = 1085))
+INNER JOIN orders ON orders.order_id = obs.order_id
+INNER JOIN drug_order od ON od.order_id = orders.order_id
+INNER JOIN person p ON p.person_id=e.patient_id
+INNER JOIN person_name n ON p.person_id=n.person_id
+INNER JOIN patient_identifier i ON i.patient_id = e.patient_id AND i.identifier_type = (SELECT patient_identifier_type_id FROM patient_identifier_type pi WHERE pi.name = 'National id')
+WHERE e.encounter_type = #{dispensing} and obs.concept_id =  #{amount_dispensed_concept_id}
+GROUP BY e.patient_id
+HAVING days_over_due >= 21
+ORDER BY e.encounter_datetime DESC,e.date_created DESC")   
+=end
   end                                                                           
                                                                                 
   def self.art_stopped_patients(start_date,end_date)                             
