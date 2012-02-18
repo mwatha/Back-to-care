ffcad1c3cfdf089207a57ab4ece658615ac0cb0f
mwatha <mwatha.bwanalie@baobabhealth.org>
2012-02-18 12:18:36 +0200

    UI - working on the UI

---
+++ db/migrate/20120214142502_patient_trace_outcome.rb
@@ -0,0 +1,18 @@
+class PatientTraceOutcome < ActiveRecord::Migration
+  def self.up
+    create_table :patient_trace_outcome do |t|                                               
+      t.interger :patient_id                                                              
+      t.interger :trace_outcome_id                                                       
+      t.date :outcome_date 
+      t.interger :creator                                                       
+      t.datetime :date_created ,:default => Time.now()
+      t.boolean :retired ,:default => false                                                  
+      t.interger :retired_by                                                    
+      t.datetime :retired_datetime                                              
+      t.string :retired_reason                                                    
+  end
+
+  def self.down
+    drop_table :patient_trace_outcome
+  end
+end
---
+++ db/migrate/20120214142611_prescribed_days_missed.rb
@@ -0,0 +1,38 @@
+class PrescribedDaysMissedFunction < ActiveRecord::Migration
+  def self.up
+ActiveRecord::Base.connection.execute <<EOF
+DROP FUNCTION IF EXISTS prescribed_days_missed;
+EOF
+
+    ActiveRecord::Base.connection.execute <<EOF
+CREATE FUNCTION prescribed_days_missed(start_date varchar(10),end_date varchar(10),dose double,quantity double) RETURNS INT 
+DETERMINISTIC
+BEGIN
+DECLARE days INT;
+DECLARE expected_remaining double;
+DECLARE days_gone_since_dispension INT;
+DECLARE run_out_date DATE;
+DECLARE sDate DATE;
+DECLARE eDate DATE;
+
+set sDate = (SELECT DATE(start_date));
+set eDate = (SELECT DATE(end_date));
+set days_gone_since_dispension = (SELECT DATEDIFF(eDate,sDate));
+set expected_remaining = (quantity - (dose*days_gone_since_dispension));
+set run_out_date = (SELECT DATE_ADD(sDate, INTERVAL (quantity/dose) DAY));            
+
+if expected_remaining >= 0 then set days = 0;
+elseif expected_remaining < 0 then set days = (SELECT DATEDIFF(eDate,run_out_date)); 
+end if;
+
+RETURN days;
+END;
+EOF
+  end
+
+  def self.down
+ActiveRecord::Base.connection.execute <<EOF
+DROP FUNCTION IF EXISTS prescribed_days_missed;
+EOF
+  end
+end
