9241c574a1f78f25a0198769c190450c625ce46c
mwatha <mwatha.bwanalie@baobabhealth.org>
2012-03-28 11:16:58 +0200

    working on UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 ActionController::Routing::Routes.draw do |map|
-  map.connect '', :controller => "home"                                
-  map.connect ':controller/:action/:id.:format'                                 
-  map.connect ':controller/:action/:id'                                         
-  map.connect ':controller/:action/'
+  # The priority is based upon order of creation: first created -> highest priority.
+  
+  # Sample of regular route:
+  # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
+  # Keep in mind you can assign values other than :controller and :action
+
+  # Sample of named route:
+  # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
+  # This route can be invoked with purchase_url(:id => product.id)
+
+  # You can have the root of your site routed by hooking up '' 
+  # -- just remember to delete public/index.html.
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
+  map.root :controller => "home" ,:actopn => "index"
 end
